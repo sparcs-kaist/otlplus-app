ae458eb285b2495671e449112e8b6f495b9a2845
Soungmin <soungmin@Soungminui-MacBookPro.local>
2026-08-25 14:34:23 +0900

    chore: add preference key and url constants

---
+++ lib/constants/preference_keys.dart
@@ -0,0 +1,15 @@
+abstract final class PreferenceKeys {
+  static const hasAccount = 'hasAccount';
+  static const notificationConsentShown = 'notification_consent_shown';
+  static const popup = 'popup';
+  static const userId = 'user_id';
+
+  static const sendCrashlytics = 'sendCrashlytics';
+  static const sendCrashlyticsAnonymously = 'sendCrashlyticsAnonymously';
+  static const sendAnalytics = 'sendAnalytics';
+  static const showsChannelTalkButton = 'showsChannelTalkButton';
+  static const sendAlarm = 'sendAlarm';
+  static const promotionAlarm = 'promotionAlarm';
+  static const informationAlarm = 'informationAlarm';
+  static const subjectSuggestionAlarm = 'subjectSuggestionAlarm';
+}
