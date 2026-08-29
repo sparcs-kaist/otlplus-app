9994e6ba98f0c25a8932e5d4cfd583a3bc855439
Soungmin <soungmin@Soungminui-MacBookPro.local>
2026-08-29 20:50:57 +0900

    test: update provider analytics fixtures
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- test/providers/settings_model_topic_test.dart
+++ test/providers/settings_model_topic_test.dart
@@ -4,16 +4,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:otlplus/providers/settings_model.dart';
-import 'package:otlplus/services/posthog_service.dart';
+import 'package:otlplus/services/channel_talk_analytics_service.dart';
 import 'package:otlplus/services/telemetry_coordinator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class _NoopAnalyticsClient implements AnalyticsClient {
   @override
   Future<void> capture(String eventName) async {}
-
-  @override
-  Future<void> identify(String distinctId) async {}
 
   @override
   Future<void> disable() async {}
